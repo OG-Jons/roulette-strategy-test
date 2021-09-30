23cbfeef9cc30020288ae50ccad46919b18f02df
OG-Jons <jonas@marschall.pro>
2021-09-30 14:05:33 +0200

    implemented roulette

---
+++ README.md
@@ -0,0 +1,24 @@
+## Our strategy tester
+You bet on a color, everytime you lost you double your betting (so if you lose with 1 you go 2 then 4 and so on)
+
+If you win, you quit immediatly and leave the table.
+
+You'll only win the amount that you bet initially.
+
+The code is in `main.ts` and typescript compiles it to `main.js`
+
+### How to run
+1. Install the latest version of node
+   1. By either installing it from https://nodejs.org/en/
+   2. If already install update with
+   ```
+   npm i -g npm@latest
+   ```
+2. Install packages
+   ```
+   npm i
+   ```
+3. Start application
+    ```
+    npm start
+   ```
--- main.js
+++ main.js
@@ -2,179 +2,214 @@ var inquirer = require('inquirer');
 var fields = {
     0: {
         number: 0,
-        color: "green",
+        color: "Green",
     },
     1: {
         number: 26,
-        color: "black",
+        color: "Black",
     },
     2: {
         number: 3,
-        color: "red",
+        color: "Red",
     },
     3: {
         number: 35,
-        color: "black",
+        color: "Black",
     },
     4: {
         number: 12,
-        color: "red",
+        color: "Red",
     },
     5: {
         number: 28,
-        color: "black",
+        color: "Black",
     },
     6: {
         number: 7,
-        color: "red",
+        color: "Red",
     },
     7: {
         number: 29,
-        color: "black",
+        color: "Black",
     },
     8: {
         number: 18,
-        color: "red",
+        color: "Red",
     },
     9: {
         number: 22,
-        color: "black",
+        color: "Black",
     },
     10: {
         number: 9,
-        color: "red",
+        color: "Red",
     },
     11: {
         number: 31,
-        color: "black",
+        color: "Black",
     },
     12: {
         number: 14,
-        color: "red",
+        color: "Red",
     },
     13: {
         number: 20,
-        color: "black",
+        color: "Black",
     },
     14: {
         number: 1,
-        color: "red",
+        color: "Red",
     },
     15: {
         number: 33,
-        color: "black",
+        color: "Black",
     },
     16: {
         number: 16,
-        color: "red",
+        color: "Red",
     },
     17: {
         number: 24,
-        color: "black",
+        color: "Black",
     },
     18: {
         number: 5,
-        color: "red",
+        color: "Red",
     },
     19: {
         number: 10,
-        color: "black",
+        color: "Black",
     },
     20: {
         number: 23,
-        color: "red",
+        color: "Red",
     },
     21: {
         number: 8,
-        color: "black",
+        color: "Black",
     },
     22: {
         number: 30,
-        color: "red",
+        color: "Red",
     },
     23: {
         number: 11,
-        color: "black",
+        color: "Black",
     },
     24: {
         number: 36,
-        color: "red",
+        color: "Red",
     },
     25: {
         number: 13,
-        color: "black",
+        color: "Black",
     },
     26: {
         number: 27,
-        color: "red",
+        color: "Red",
     },
     27: {
         number: 6,
-        color: "black",
+        color: "Black",
     },
     28: {
         number: 34,
-        color: "red",
+        color: "Red",
     },
     29: {
         number: 17,
-        color: "black",
+        color: "Black",
     },
     30: {
         number: 25,
-        color: "red",
+        color: "Red",
     },
     31: {
         number: 2,
-        color: "black",
+        color: "Black",
     },
     32: {
         number: 21,
-        color: "red",
+        color: "Red",
     },
     33: {
         number: 4,
-        color: "black",
+        color: "Black",
     },
     34: {
         number: 19,
-        color: "red",
+        color: "Red",
     },
     35: {
         number: 15,
-        color: "black",
+        color: "Black",
     },
     36: {
         number: 32,
-        color: "red",
+        color: "Red",
     },
 };
-var question = [
+var questionsGame = [
     {
         type: 'input',
         name: 'startAmount',
         message: 'Starting amount: ',
-        default: 1000
+        default: 100
     },
     {
         type: 'input',
         name: 'startBet',
         message: 'Starting bet: ',
         default: 1
+    },
+    {
+        type: 'list',
+        name: 'chosenColor',
+        message: 'Which color do you want to use?',
+        choices: [
+            'Red',
+            'Black'
+        ],
+        default: 0
     }
 ];
-inquirer
-    .prompt(question)
-    .then(function (answers) {
-    var amount = parseInt(answers.startAmount);
-    var bet = parseInt(answers.startBet);
-    var currentBet = bet;
-    console.log(amount);
-    while (amount > 0) {
-        console.log({ amount: amount, startBet: bet });
-        amount -= bet;
-    }
-});
-function (max) {
-    return Math.floor(Math.random() * max);
+function gameFunction() {
+    inquirer
+        .prompt(questionsGame)
+        .then(function (answers) {
+        var amount = parseInt(answers.startAmount);
+        var startingAmount = parseInt(answers.startAmount);
+        var startingBet = parseInt(answers.startBet);
+        var chosenColor = answers.chosenColor;
+        var amountOfTries = 0;
+        var currentBet = startingBet;
+        while (amount > 0) {
+            if (amount === (startingAmount + startingBet))
+                break;
+            if (amount < currentBet)
+                break;
+            amountOfTries++;
+            amount -= currentBet;
+            currentBet *= 2;
+            var random = randomNumber(37);
+            var result = fields[random];
+            if (result.color === chosenColor) {
+                amount += currentBet;
+                break;
+            }
+            else {
+                console.log(amountOfTries + ". Didn't hit " + chosenColor + " (hit " + result.color + " with the number " + result.number + ") and lost " + currentBet + " (total is now " + amount + ")");
+            }
+        }
+        if (amount === (startingAmount + startingBet)) {
+            console.log("You won at attempt nr. " + amountOfTries + " and have a total of " + amount + ", quit right now");
+        }
+        else if (amount < startingAmount) {
+            console.log("You lost and cant get your bettings backs.");
+        }
+    });
 }
+function randomNumber(max) {
+    return Math.floor(Math.random() * (max));
+}
+gameFunction();
